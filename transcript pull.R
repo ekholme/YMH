1692f88b1a787e01e5a61ed1b935d47474a0c72c
ekholme <eric.ekholm@gmail.com>
2020-05-15 09:52:53 -0400

    finished defining words viz

--- transcript pull.R
+++ transcript pull.R
@@ -4,11 +4,16 @@ library(youtubecaption)
 library(janitor)
 library(lubridate)
 
+#links to youtube urls from following website:
+#http://www.williamsportwebdeveloper.com/FavBackUp.aspx
+#pulled on 5/14/20
+
 #reading in list of youtube urls and getting automatic transcripts
-ep_links <- read_csv(here::here("Data/ymh_full_ep_links.csv")) %>%
+ep_links <- read_csv(here::here("Data/ymh_ep_links.csv")) %>%
   clean_names() %>%
-  mutate(ep_num = str_remove_all(title, "\\D+") %>%
-           as.integer(),
+  mutate(ep_num = str_replace_all(title, ".*Ep.*(\\d{3}).*", "\\1") %>%
+           as.double(),
+         ep_num = if_else(ep_num == 19, 532, ep_num),
          published_date = mdy_hm(published_date),
          vid = str_replace_all(video_url, ".*=(.*)$", "\\1"))
 
@@ -26,4 +31,4 @@ res <- map(1:length(ymh_trans),
             by = "vid")
 
 #saving transcripts out to csv so I don't need to grab them again
-write_csv(res, here::here("Data/ymh_transcripts_raw.csv"))
+write_csv(res, here::here("Data/ymh_transcripts_raw2.csv"))
